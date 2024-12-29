9dedf19e3aaf90371819d4f9c7d4562480f6296c
Rodrigo Fisch <rodrigo.fisch@capgemini.com>
2024-12-29 16:29:24 -0300

    [ADRTC0001} - Criação e finalização do metodos update, delete, consult e list dos pacotes controller e service e implementação dos pacotes configurations e repository

--- src/main/java/com/rodrigofisch/easyregistration/repository/RegisterPersonRepository.java
+++ src/main/java/com/rodrigofisch/easyregistration/repository/RegisterPersonRepository.java
@@ -4,6 +4,9 @@ import com.rodrigofisch.easyregistration.domain.RegisterPerson;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegisterPersonRepository extends MongoRepository<RegisterPerson, String> {
+    Optional<RegisterPerson> findByCpf(String cpf);
 }
